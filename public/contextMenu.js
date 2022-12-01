c42304c441a4b38895f047f50b113c168633117d
Z-ls <dragonzhao1992@gmail.com>
2022-12-01 20:23:50 +0100

    Show Picture in popup + (Possibly temporary) solution of passing data

--- public/contextMenu.js
+++ public/contextMenu.js
@@ -1,13 +1,11 @@
 /* global chrome*/
-chrome.contextMenus.create(
-	{
-		id: "savimages",
-		title: "Open a New Tab for this picture",
-		contexts: ["video", "image"]
-	},
-	() => {
-		chrome.contextMenus.onClicked.addListener((info, tab) => {
-			chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
-		});
-	}
-);
+chrome.contextMenus.create({
+	id: "savimages",
+	title: "Open a New Tab for this picture",
+	contexts: ["image"]
+});
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+	await chrome.storage.sync.set({ picSrcUrl: info.srcUrl });
+	await chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
+});
--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 /*global chrome*/
 import "./App.css";
-import { MemoryRouter as Router, Route, Routes } from "react-router-dom";
-import { MainPage } from "./MainPage";
+import { useState, useEffect } from "react";
+import { Container, Row, Col, Figure, Button } from "react-bootstrap";
 
 function App() {
+	const [url, setUrl] = useState("");
+	const [srcUrl, setSrcUrl] = useState();
+	const [showUrl, setShowUrl] = useState(false);
+	useEffect(() => {
+		chrome.storage.sync.get(["picSrcUrl"], function (data) {
+			setSrcUrl(data.picSrcUrl);
+		});
+		chrome.tabs &&
+			chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+				setUrl(tabs.find(tab => tab.url.includes("yande")).url);
+			});
+	}, []);
+
 	return (
-		<Router>
-			<Routes>
-				<Route index element={<MainPage />} />
-			</Routes>
-		</Router>
+		<Container fluid>
+			{showUrl && (
+				<Row className="my-5">
+					<Col className="d-flex justify-content-center">{url ? <h1>{url}</h1> : <h1>TEST</h1>}</Col>
+				</Row>
+			)}
+			<Row className="my-5">
+				<Col className="d-flex justify-content-center">
+					<Figure>
+						<Figure.Image width={768} height={480} src={srcUrl} />
+						<Figure.Caption>{srcUrl}</Figure.Caption>
+					</Figure>
+				</Col>
+			</Row>
+			<Row className="my-4">
+				<Col className="d-flex justify-content-center">
+					<Button
+						variant="primary"
+						onClick={() => {
+							setShowUrl(show => !show);
+						}}>
+						Check latest Page
+					</Button>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
 
